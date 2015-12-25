e7af280035e8608dde82b3ccb6a7a80b8c076720
Grant Kemp <grant@kempnet.com>
2015-12-25 00:46:20 +0000

    Moved Constants and index to separate file to make it a bit tidier and easy to understand

---
+++ Algolia/AlgoliaIndexConstants.swift
@@ -0,0 +1,23 @@
+//
+//  AlgoliaIndexConstants.swift
+//  Algolia
+//
+//  Created by Grant Kemp on 25/12/2015.
+//  Copyright © 2015 Grant Kemp. All rights reserved.
+//
+
+import Foundation
+
+extension Constants {
+    struct AlgoliaIndexConstants {
+        static let INDEX = "getstarted_actors" //If you put the wrong index in - Algolia doesn't give an error message
+        static let objectID = "objectID"
+        static let name = "name"
+        static let rating = "rating"
+        static let imagePath = "image_path"
+        static let alternative_name = "alternative_name"
+}
+    
+
+
+}
